015a83ac2562f1f6454d3f880af7820ef3e843f5
Salman Zafar <maanizfar@gmail.com>
2020-04-04 01:45:21 +0500

    created CustomUserAdmin

--- accounts/admin.py
+++ accounts/admin.py
@@ -1,3 +1,18 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
 
-# Register your models here.
+from accounts.models import CustomUser
+
+
+class CustomUserAdmin(UserAdmin):
+    list_display = ('username', 'email', 'date_joined',
+                    'last_login', 'is_admin', 'is_staff',)
+    readonly_fields = ('date_joined', 'last_login')
+    search_fields = ('username', 'email')
+
+    filter_horizontal = ()
+    fieldsets = ()
+    list_filter = ()
+
+
+admin.site.register(CustomUser, CustomUserAdmin)
