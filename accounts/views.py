ceae38014f0214e036988fa29f6e4b7625cc454c
Salman Zafar <maanizfar@gmail.com>
2020-04-04 01:33:04 +0500

    created SignUpView

--- accounts/views.py
+++ accounts/views.py
@@ -1,3 +1,10 @@
 from django.shortcuts import render
+from django.views.generic import CreateView
 
-# Create your views here.
+from accounts.forms import SignUpForm
+
+
+class SignUpView(CreateView):
+    form_class = SignUpForm
+    template_name = 'accounts/signup.html'
+    success_url = '/'
