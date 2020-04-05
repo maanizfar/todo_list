3ba9d278d9bdb54844989e0f7eea4e8f51f97792
Salman Zafar <maanizfar@gmail.com>
2020-04-05 21:51:48 +0500

    added CRUD to Task

--- todo_app/models.py
+++ todo_app/models.py
@@ -1,3 +1,11 @@
 from django.db import models
+from django.contrib.auth import get_user_model
 
-# Create your models here.
+
+class Task(models.Model):
+    text = models.TextField(blank=False, null=False, max_length=200)
+    author = models.ForeignKey(get_user_model(), on_delete=models.CASCADE)
+    time_created = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.text
